b80eaaa8030fbdbf388efc6fc80846a1c48b077c
vedantkherade <152008775+vedantkherade@users.noreply.github.com>
2024-07-20 17:07:00 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,108 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+
+   <link rel="stylesheet" href="css/style.css">
+   <style>
+     <?php include "css/style.css" ?>
+   </style>
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>We are the gateway to educational excellence. Choose us for a team of dedicated educators, a commitment to affordability, and a history of student success. Trust us to be your partner on your path to achievement</p>
+         <a href="courses.php" class="inline-btn">our courses</a>
+      </div>
+
+   </div>
+
+   <div class="box-container">
+
+      <div class="box">
+         <i class="fas fa-graduation-cap"></i>
+         <div>
+            <h3>+50</h3>
+            <span>online courses</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-user-graduate"></i>
+         <div>
+            <h3>+500</h3>
+            <span>brilliants students</span>
+         </div>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-chalkboard-user"></i>
+         <div>
+            <h3>+50</h3>
+            <span>expert teachers</span>
+         </div>
+      </div>
+
+     
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+
+<script src="js/script.js"></script>
+   
+</body>
+</html>
